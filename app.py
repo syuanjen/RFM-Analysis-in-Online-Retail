add8b0ecfcb5d38382a380b606c967d2cb56fc7c
Sabrina Chen <syuanjenchen@gmail.com>
2025-11-18 07:27:20 +0000

    upload stream app

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Hello Streamlit!")
+st.write("Welcome to your first Streamlit App 😎")
+
+if st.button("Click me!"):
+    st.success("You clicked the button successfully!")
